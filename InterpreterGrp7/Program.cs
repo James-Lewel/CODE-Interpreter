7b4a6c06062d7447c0781d3199e073df0326ef2d
James Lewel Padecio <jameslewel.padecio@cit.edu>
2023-03-05 16:57:16 +0800

    Replaced previous project with current project

---
+++ InterpreterGrp7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterpreterGrp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var path = Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory()));
+            Console.WriteLine(path);
+
+            Console.ReadLine();
+        }
+    }
+}
